40c89336f46d2c010728501d942a98c8096ae12c
thomas vanhoutte <thomas.vanhoutte@student.howest.be>
2018-08-15 23:09:30 +0200

    CEA-1: Adding a login page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { EventsOverviewComponent } from './modules/events/containers/events-overview/events-overview.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { LoginComponent } from './modules/login/containers/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventsOverviewComponent
+    EventsOverviewComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
